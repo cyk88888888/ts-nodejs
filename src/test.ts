9811a36ba59236cbc59732df8ef19105b22f36be
cyk <chenyangkun@modo.cn>
2022-02-28 14:52:46 +0800

    删除无用模块

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,5 @@
-console.log('Your cli is running. And now it is recompiled automatically！');
-
 const a = 0;
 const b = 0;
 const c = 0;
 const d = 0;
+console.log('Your cli is running. And now it is recompiled automatically！');
