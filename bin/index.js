d8c3b975635dd488d6b89b52432be2e78b9bd76e
cyk <chenyangkun@modo.cn>
2022-02-28 17:27:17 +0800

    修改

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-console.log("nodejs跑起来了！！！！");
+const exec = require('child_process').exec;
+const path = require('path');
+let root_dir = __dirname.split('bin')[0];
+let tsconfig_dir = root_dir + 'src';
+exec(root_dir + 'node_modules/.bin/tsc-plus -p ' + tsconfig_dir, {encoding: 'utf8'}, function (err, stdout, stderr) {
+    if (err) {
+        console.log(err);
+        console.log('stderr:' + stderr);
+        return;
+    }
+    console.log('编译typescript完成！！！！！！');
+});
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-const a = 0;
-const b = 0;
-const c = 0;
-const d = 0;
-console.log('Your cli is running. And now it is recompiled automatically！');
